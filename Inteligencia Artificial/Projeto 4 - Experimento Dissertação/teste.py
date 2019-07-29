67d4c1b8369cc0ac13dcb0509acd91c578d69267
João Marcos Mareto Calado <joaomarcosmareto@gmail.com>
2019-07-29 15:13:01 -0300

    Atualização do código para limpar o arquivo principal

--- Inteligencia Artificial/Projeto 4 - Experimento Dissertação/teste.py
+++ Inteligencia Artificial/Projeto 4 - Experimento Dissertação/teste.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 import metricas
 import organiza_datasets
-import numpy
+import core
 
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
 
 input_file = 'teste.json'
 input_encoding = 'utf8'
@@ -16,6 +15,8 @@ input_encoding = 'utf8'
 dataset         = []
 labels          = []
 caracteristicas = []
+
+rfc_testar_hiperparametros = False
 
 # cria o dataset, lendo o json (input_file), removendo os registros com dados nulos ou string vazia;
 # cria instancias negativas e concatena num dataset único;
@@ -36,53 +37,38 @@ caracteristicas = metricas.extrair_caracteristicas(dataset)
 #X_train,     X_test,      y_train,           y_test =           train_test_split(X,               y,)
 treino_dados, teste_dados, treino_resultados, teste_resultados = train_test_split(caracteristicas, labels, test_size=0.30)
 
-# BEGIN - TRECHO TESTADO EM UM COMPUTADOR COM 16 CORES, E 8 GIGAS DE RAM
-# Testa para descobrir o melhor conjunto de hiper-parâmetros para o classificador
-# número de árvores na floresta aleatória
-n_estimators = [int(x) for x in numpy.linspace(start = 200, stop = 2000, num = 10)]
-# número de características em cada split
-max_features = ['auto', 'sqrt']
+# obtém a predição a partir da árvore de decisão
+pred_decision_tree = core.get_predicao_decision_tree(treino_dados, treino_resultados, teste_dados)
 
-# profundidade máxima
-max_depth = [int(x) for x in numpy.linspace(100, 500, num = 11)]
-max_depth.append(None)
-# create random grid
-random_grid = {
-    'n_estimators' : n_estimators,
-    'max_features' : max_features,
-    'max_depth'    : max_depth
-}
-# Random search of parameters
-modelo = RandomForestClassifier()
-rfc_random = RandomizedSearchCV(estimator = modelo, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
-# Fit the model
-rfc_random.fit(treino_dados, treino_resultados)
-# END - TRECHO TESTADO EM UM COMPUTADOR COM 16 CORES, E 8 GIGAS DE RAM
+#obtém a predição a partir da random forest
+pred_random_forest = core.get_predicao_random_forest(rfc_testar_hiperparametros, treino_dados, treino_resultados, teste_dados)
 
-# Constrói o classificador com os dados do teste de tunning de hiper-parâmetros
-modelo = RandomForestClassifier(
-        n_estimators = rfc_random.best_params_['n_estimators'],
-        max_depth    = rfc_random.best_params_['max_depth'],
-        max_features = rfc_random.best_params_['max_features'],
-)
-
-# Preenche o modelo com dados e labels de treino
-modelo.fit(treino_dados, treino_resultados)
-
-# Usa o conjunto de dados de teste para tentar prever os resultados
-pred = modelo.predict(teste_dados)
+#faz o relatório contendo matriz de confusao e métricas precisao, sensibilidade (recall), f1-score e acurácia
+#a = core.report(teste_resultados, pred_decision_tree)
 
 # Resultado de Cross Validation
-cv_score = cross_val_score(modelo, caracteristicas, labels, cv=10, scoring='roc_auc')
+#cv_score = cross_val_score(modelo, caracteristicas, labels, cv=10, scoring='roc_auc')
 
 print("=== Confusion Matrix ===")
-print(confusion_matrix(teste_resultados, pred))
+print(confusion_matrix(teste_resultados, pred_decision_tree))
 print('\n')
 print("=== Classification Report ===")
-print(classification_report(teste_resultados, pred))
+print(classification_report(teste_resultados, pred_decision_tree))
+print('\n')
+#print("=== All AUC Scores ===")
+#print(cv_score)
+print('\n')
+#print("=== Mean AUC Score ===")
+#print("Mean AUC Score - Random Forest: ", cv_score.mean())
+
+print("=== Confusion Matrix ===")
+print(confusion_matrix(teste_resultados, pred_random_forest))
+print('\n')
+print("=== Classification Report ===")
+print(classification_report(teste_resultados, pred_random_forest))
 print('\n')
 print("=== All AUC Scores ===")
-print(cv_score)
+#print(cv_score)
 print('\n')
-print("=== Mean AUC Score ===")
-print("Mean AUC Score - Random Forest: ", cv_score.mean())
+#print("=== Mean AUC Score ===")
+#print("Mean AUC Score - Random Forest: ", cv_score.mean())
